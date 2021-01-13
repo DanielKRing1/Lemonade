1171c58e3658c174bf90053bec284895e0ec9fec
DanielKRing1 <DanielKRing1@gmail.com>
2021-01-13 00:48:08 -0800

    Add beginning of Thunk and Reducer case to Rate Day

--- src/redux/slices/daysTracker.ts
+++ src/redux/slices/daysTracker.ts
@@ -1,36 +1,69 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createAsyncThunk, createSelector, createSlice, PayloadAction} from '@reduxjs/toolkit';
 
-import { DayPart } from './dayPartTracker';
+import {DayPart} from './dayPartTracker';
 
 export type Day = Array<DayPart>;
 export type Days = Array<Day>;
 
 export type DaysTrackerState = {
-    local: Days;
-    server: Days;
+  days: Record<string, Array<any>>;
 };
 const initialLifeTrackerState: DaysTrackerState = {
-    local: [],
-    server: [],
+  days: {},
 };
-export const daysTrackerSlice = createSlice({
-    name: 'daysTracker',
-    initialState: initialLifeTrackerState,
-    reducers: {
-        rateDay(state, action: { payload: { mood: string; moodIntensity: string; activities: Array<string>; } }) {
-            const { mood, moodIntensity, activities } = action.payload;
-            const dayPart: DayPart = {
-                mood,
-                moodIntensity,
-                activities
-            };
 
-            const lastDayIndex = state.local.length - 1;
-            state.local[lastDayIndex].push(dayPart);
-        }
+// THUNKS
+
+const rateDay = createAsyncThunk(
+  'daysTracker/rateDay',
+  async (args: {mood: string; moodIntensity: string; activities: Array<string>}, {dispatch, getState}): Promise<any> => {
+    try {
+      // TODO
+      // Get TrendTracker
+      //   const defaultRealm = RealmCache.getDefaultRealm();
+      //   const schemaBlueprints: Realm.Results<SchemaBlueprintRow> = await defaultRealm.objects(RealmSchemaName.SchemaBlueprint);
+
+      // Rate in TrendTracker -> Writes rating to Realm
+
+      return args;
+    } catch (err) {
+      console.log(`RealmCache._loadSchemaBlueprints error: ${err}`);
+
+      return [];
     }
+  },
+);
+
+export {rateDay};
+
+// SLICE
+
+export const daysTrackerSlice = createSlice({
+  name: 'daysTracker',
+  initialState: initialLifeTrackerState,
+  reducers: {},
+  extraReducers: {
+    [rateDay.fulfilled]: (state, action: {payload: {mood: string; moodIntensity: string; activities: Array<string>}}) => {
+      // Format DayPart
+      const {mood, moodIntensity, activities} = action.payload;
+      const dayPart: DayPart = {
+        date: new Date().toDateString(),
+        mood,
+        moodIntensity,
+        activities,
+      };
+
+      // Today's date
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+
+      // Store in Redux: DayPart under today's date
+      if (!state.days.hasOwnProperty(today.toDateString())) state.days[today.toDateString()] = [];
+      state.days[today.toDateString()].push(dayPart);
+    },
+  },
 });
 
-export const { rateDay } = daysTrackerSlice.actions;
+// export const {rateDay} = daysTrackerSlice.actions;
 
 export default daysTrackerSlice.reducer;
