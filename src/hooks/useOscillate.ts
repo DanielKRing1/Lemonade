53b1efbf343bb071b5789a5e60f9e248d9775ce0
DanielKRing1 <DanielKRing1@gmail.com>
2021-02-06 23:45:47 -0800

    Modularized 'animating'

--- src/hooks/useOscillate.ts
+++ src/hooks/useOscillate.ts
@@ -1,81 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import Animated, { Easing } from 'react-native-reanimated';
+import React, {useState, useEffect} from 'react';
+import Animated, {Easing} from 'react-native-reanimated';
 
 const MIN_TO_VAL = 0;
 const MAX_TO_VAL = 1;
 
 type OptionParams = {
-    duration?: number;
-    initIsMax?: boolean;
-    hasListener?: boolean;
+  duration?: number;
+  initIsMax?: boolean;
+  hasListener?: boolean;
 };
-export const useOscillate = (min: number, max: number, { duration = 500, initIsMax = false, hasListener = true }: OptionParams) => {
+export const useOscillate = (min: number, max: number, {duration = 500, initIsMax = false, hasListener = true}: OptionParams) => {
+  const [animation, setAnimation] = useState<Animated.Value<number>>(new Animated.Value(initIsMax ? MAX_TO_VAL : MIN_TO_VAL));
+  const [willMax, setWillMax] = useState<boolean>(initIsMax);
+  const [isMax, setIsMax] = useState<boolean>(initIsMax);
+  // const [value, setValue] = useState<number>(initIsMax ? MAX_TO_VAL : MIN_TO_VAL);
 
-    const [animation, setAnimation] = useState<Animated.Value<number>>(new Animated.Value(initIsMax ? MAX_TO_VAL : MIN_TO_VAL));
-    const [willMax, setWillMax] = useState<boolean>(initIsMax);
-    const [isMax, setIsMax] = useState<boolean>(initIsMax);
-    // const [value, setValue] = useState<number>(initIsMax ? MAX_TO_VAL : MIN_TO_VAL);
+  // if (hasListener) {
+  //     animation.addListener(({ value: v }) => setValue(v));
+  // }
 
-    // if (hasListener) {
-    //     animation.addListener(({ value: v }) => setValue(v));
-    // }
+  const _animate = (animateUp: boolean) => {
+    Animated.timing(animation, {
+      toValue: animateUp ? MAX_TO_VAL : MIN_TO_VAL,
+      duration,
+      easing: Easing.in(Easing.ease),
+    }).start(() => {
+      setIsMax((prevIsMax) => animateUp);
+    });
 
-    const oscillate = () => {
-        Animated.timing(animation, {
-            toValue: isMax ? MIN_TO_VAL : MAX_TO_VAL,
-            duration,
-            easing: Easing.in(Easing.ease),
-        }).start(() => {
-            setIsMax(prevIsMax => !prevIsMax);
-        });
+    setWillMax((prevWillMax) => animateUp);
+  };
 
-        setWillMax(prevWillMax => !prevWillMax);
-    };
+  const oscillate = () => {
+    // TODO May be useful?
+    // if (willMax !== isMax) return;
 
-    const animateUp = () => {
-        console.log('a');
-        if (willMax) return;
+    const animateUp = !isMax;
+    _animate(animateUp);
+  };
 
-        console.log('b');
+  const animateUp = () => {
+    console.log('a');
+    if (willMax) return;
 
-        Animated.timing(animation, {
-            toValue: MAX_TO_VAL,
-            duration,
-            easing: Easing.in(Easing.ease),
-        }).start(() => {
-            setIsMax(true);
-        });
+    console.log('b');
 
-        setWillMax(true);
-    };
+    _animate(true);
+  };
 
-    const animateDown = () => {
-        if (!willMax) return;
+  const animateDown = () => {
+    if (!willMax) return;
 
-        Animated.timing(animation, {
-            toValue: MIN_TO_VAL,
-            duration,
-            easing: Easing.in(Easing.ease),
-        }).start(() => {
-            setIsMax(false);
-        });
+    _animate(false);
+  };
 
-        setWillMax(false);
-    };
-
-    return {
-        isMax,
-        willMax,
-        animation,
-        // value,
-        interpolation: animation.interpolate({
-            inputRange: [MIN_TO_VAL, MAX_TO_VAL],
-            outputRange: [min, max],
-        }),
-        min: MIN_TO_VAL,
-        max: MAX_TO_VAL,
-        animate: oscillate,
-        animateUp,
-        animateDown
-    };
+  return {
+    isMax,
+    willMax,
+    animation,
+    // value,
+    interpolation: animation.interpolate({
+      inputRange: [MIN_TO_VAL, MAX_TO_VAL],
+      outputRange: [min, max],
+    }),
+    min: MIN_TO_VAL,
+    max: MAX_TO_VAL,
+    animate: oscillate,
+    animateUp,
+    animateDown,
+  };
 };
