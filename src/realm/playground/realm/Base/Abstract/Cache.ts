8b60c870827e0fc8cce730ea5192c0166c13acaf
DanielKRing1 <DanielKRing1@gmail.com>
2021-03-28 23:05:55 -0700

    Normalize schema data structure with private caches nested withinin SchemaCache These Cache reference an array of schemaNames, which can be looked up from the outer SchemaCache

--- src/realm/playground/realm/Base/Abstract/Cache.ts
+++ src/realm/playground/realm/Base/Abstract/Cache.ts
@@ -1,7 +1,8 @@
 import {NotInCacheError, NotImplementedError} from '../../Errors';
+import {Override} from '../Decorators';
 
 export class Cache<T> {
-  protected _map: Record<string, T>;
+  protected _map: Dict<T>;
 
   constructor() {
     this._map = {};
@@ -17,7 +18,7 @@ export class Cache<T> {
     throw NotImplementedError({});
   }
 
-  rm(key: string, options?: any): T | undefined {
+  rm(key: string, options?: any): T | undefined | void {
     if (this.has(key)) {
       const val = this._map[key];
       delete this._map[key];
@@ -26,7 +27,7 @@ export class Cache<T> {
     }
   }
 
-  get(key: string): T {
+  get(key: string): T | undefined {
     if (this.has(key)) return this._map[key];
 
     // throw NotInCacheError({message: `"${key}" does not exist in ${this.constructor} Cache`});
@@ -34,5 +35,34 @@ export class Cache<T> {
 
   has(key: string) {
     return this._map.hasOwnProperty(key);
+  }
+}
+
+export class ArrayCache<T> extends Cache<T[]> {
+  protected _map: Dict<T[]>;
+
+  constructor() {
+    super();
+
+    this._map = {};
+  }
+
+  @Override('Cache')
+  rm(key: string, itemToRm: T, options?: any): void {
+    if (this.has(key)) {
+      const arr = this._map[key];
+      const indexToRm = typeof itemToRm === 'object' ? arr.findIndex((item: Object) => item.toString() === (itemToRm as Object).toString()) : arr.findIndex((item: T) => item === itemToRm);
+      this._map[key] = arr.splice(indexToRm, 1);
+    }
+  }
+
+  protected addToArr(key: string, value: T) {
+    this.initArray(key);
+
+    this._map[key].push(value);
+  }
+
+  protected initArray(key: string) {
+    if (!this.has(key)) this._map[key] = [];
   }
 }
--- src/realm/playground/realm/Realm/Caches/SchemaCache.ts
+++ src/realm/playground/realm/Realm/Caches/SchemaCache.ts
@@ -1,20 +1,51 @@
-import {Cache, Singleton} from '../../Base';
+import {sceneName} from 'aws-amplify';
+import {ArrayCache, Cache, Singleton, Override} from '../../Base';
 import {SchemaBlueprint} from '../Schema/SchemaBlueprint';
 
 export class SchemaCache extends Singleton(Cache)<SchemaBlueprint> {
-  protected _realmMap: Dict<SchemaName>;
+  protected _realmMap: RealmSchemaCache = new RealmSchemaCache();
+  protected _schemaTypeMap: SchemaTypeCache = new SchemaTypeCache();
 
   constructor() {
     super();
 
-    this._realmMap = {};
-
     return this.getSingleton() as SchemaCache;
   }
 
+  @Override('Cache')
   add(schemaName: string, valueParams: Record<string, any> & {schemaBlueprint: SchemaBlueprint}) {
     const {schemaBlueprint} = valueParams;
+    const {realmPath, schemaType} = schemaBlueprint;
 
     this._map[schemaName] = schemaBlueprint;
+
+    this._realmMap.add(realmPath, {schemaName});
+    this._schemaTypeMap.add(schemaType, {schemaName});
+  }
+}
+
+class RealmSchemaCache extends Singleton(ArrayCache)<SchemaName> {
+  constructor() {
+    super();
+
+    return this.getSingleton() as RealmSchemaCache;
+  }
+
+  add(realmPath: string, valueParams: Record<string, any> & {schemaName: SchemaName}) {
+    const {schemaName} = valueParams;
+    this.addToArr(realmPath, schemaName);
+  }
+}
+
+class SchemaTypeCache extends Singleton(ArrayCache)<SchemaName> {
+  constructor() {
+    super();
+
+    return this.getSingleton() as SchemaTypeCache;
+  }
+
+  add(schemaType: string, valueParams: Record<string, any> & {schemaName: SchemaName}) {
+    const {schemaName} = valueParams;
+    this.addToArr(schemaType, schemaName);
   }
 }
