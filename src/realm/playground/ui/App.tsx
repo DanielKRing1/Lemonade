afa6b5411121f169ff1c53c2ff6f2b566b8ff1b9
DanielKRing1 <DanielKRing1@gmail.com>
2021-05-10 21:38:06 -0700

    Add app entry point and redux store

--- src/realm/playground/ui/App.tsx
+++ src/realm/playground/ui/App.tsx
@@ -1,10 +1,15 @@
 import 'react-native-gesture-handler';
-import {Navigation} from '@react-navigation/native';
 
+// Redux store
+import {Provider} from 'react-redux';
+import store from '../redux/store';
+
+// For registering app
 import {AppRegistry, Platform} from 'react-native';
 import {name as appName} from '../../../../app.json';
 
 import AppNav from './AppNav';
+import {FC} from 'react';
 
 // Platform specific setup
 switch (Platform.OS) {
@@ -17,6 +22,12 @@ switch (Platform.OS) {
     break;
 }
 
-// Register app
-const App = AppNav;
+// 1. Add Redux to React UI
+const App: FC = (props) => (
+  <Provider store={store}>
+    <AppNav />
+  </Provider>
+);
+
+// 2. Register app
 AppRegistry.registerComponent(appName, () => App);
